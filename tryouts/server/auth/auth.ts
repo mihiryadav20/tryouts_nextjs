45505e1f32d82d0c6797021b5f2726c503131230
mihiryadav20 <80512042+mihiryadav20@users.noreply.github.com>
2025-07-18 16:32:16 +0530

    next changes

--- tryouts/server/auth/auth.ts
+++ tryouts/server/auth/auth.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { User } from '../models/prisma/index';
+import { User } from '../models/User';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
